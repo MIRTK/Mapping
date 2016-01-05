e9d0267c2731cb58776e6811fcedfc78490c8196
Andreas Schuh <andreas.schuh.84@gmail.com>
2016-01-05 15:28:03 +0000

    cmake: Set version of external module

--- BasisProject.cmake
+++ BasisProject.cmake
@@ -44,6 +44,8 @@ basis_project (
   # meta-data
   NAME        "VolumetricMapping"
   PACKAGE     "MIRTK"
+  VERSION     "1.0.0" # External module version
+  SOVERSION   "0"     # API yet unstable
   AUTHORS     "Andreas Schuh"
   DESCRIPTION "VolumetricMapping library of the Medical Image Registration ToolKit."
   COPYRIGHT   "2013-2015 Imperial College London, Andreas Schuh"
