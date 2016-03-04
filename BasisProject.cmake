1d6c51b4ae4cb247e5ecd25719457b37bb373b3e
Andreas Schuh <andreas.schuh.84@gmail.com>
2016-03-04 11:23:57 +0000

    fix: Set also SOVERSION of external module

--- BasisProject.cmake
+++ BasisProject.cmake
@@ -43,7 +43,8 @@ basis_project (
   # ----------------------------------------------------------------------------
   # meta-data
   NAME        "VolumetricMapping"
-  VERSION     "1.1.0"
+  VERSION     "1.1.0" # version of this module
+  SOVERSION   "0"     # API yet unstable
   PACKAGE     "MIRTK"
   AUTHORS     "Andreas Schuh"
   DESCRIPTION "Volumetric Mapping library of the Medical Image Registration ToolKit."
